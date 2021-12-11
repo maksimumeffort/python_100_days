f60ebd722a533fa5cba87fafe5a241336f4209c2
Alex M <alexmaksimets@gmail.com>
2021-12-11 12:55:49 +1000

    day 11 challenge edited and completed according to instructions

--- day_11_challenge.py
+++ day_11_challenge.py
@@ -1,120 +1,87 @@
-############### Blackjack Project #####################
-
-############### Our Blackjack House Rules ####################
-
-## The deck is unlimited in size. 
-## There are no jokers. Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## Use the following list as the deck of cards:
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-## The computer is the dealer.
-
-########### Setup Code ##############
-
 import random
 
-dealer_cards = []
-player_cards = []
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
-##################### Functions #####################
-
-def draw_card(side):
-    """Returns random card from the deck"""
-    random_card = random.choice(cards)
-    if random_card == 11:
-        side.append(random_card)
-        if sum(side) > 21:
-            #side.index(11) = 1 returns: SyntaxError: can't assign to function call
-            side.pop()
-            side.append(1)
-        else:
-            return
-    side.append(random_card)
-
-def init_cards():
-    """Populates the initial array of cards for dealer and player"""
-    for n in range(2):
-        draw_card(dealer_cards)
-        draw_card(player_cards)
-
-def player_draw():
-    """Puts player in a loop asking if they want to draw a card"""
-    loop = True
-    print(f'Your cards: {player_cards}, current score: {sum(player_cards)}')
-    while loop == True:
-        choice = input("Type 'y' to get another card, type 'n' to pass: ").lower()
-        if choice == 'y':
-            draw_card(player_cards)
-        else:
-            loop = False
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
+#and returns the score. 
+#Look up the sum() function to help you do this.
     
-    blackjack_check(player_cards)
-
-def dealer_draw():
-    """Makes sure dealer's score is not less than 17"""
-    while sum(dealer_cards) < 17:
-        draw_card(dealer_cards)
-        blackjack_check(dealer_cards)
-        
-def blackjack_check(side):
-    """Checks if the side given has a blackjack"""
-    if sum(side) == 21:
-        return 0
+def calculate_score(cards):
+    return sum(cards)
     
-def who_won(player_score, dealer_score):
-    """Determines the winning side"""
-    if player_score > dealer_score:
-        print('You win')
-    elif player_score < dealer_score:
-        print('You lose')
+#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "You lose, dealer has Blackjack"
+    elif user_score == 0:
+        return "You win with a Blackjack"
+    elif user_score > 21:
+        return "You went over, you lose"
+    elif computer_score > 21:
+        return "Dealer went over, you win"
+    elif user_score > computer_score:
+        return "You win"
     else:
-        print('It\'s a draw')
+        return "You lose"
 
-def show_score(condition, player, dealer):
-    """Shows user the score based on the condition"""
-    if condition == 'normal':
-        player_hand = "Your cards"
-        dealer_hand = "first card"
-        final = ""
-    elif condition == 'final':
-        player_hand = "final_hand"
-        dealer_hand = player_hand
-        final = f', final score: {sum(dealer_cards)}'
+#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
+    if total == 21 and len(cards) == 2:
+        return 0
+
+#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
     
-    print(f'Your {player_hand}: {player_cards}, current score: {sum(player_cards)}')
-    print(f'Computer\'s {dealer_hand}: {dealer_cards[0]}{final}')
-
-##################### Main Program #####################
+    return total
 
 def play_game():
-    """Blackjack application function"""
-    playing = True
-    while playing:
-        new_game = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
-        if new_game == 'y':
-            # populate the dealer and player cards
-            init_cards()
-            show_score('normal', player_cards, dealer_cards)
-            if sum(dealer_cards) < 17:
-                dealer_draw()
-            if player_draw() == 0:
-                show_score('final', player_cards, dealer_cards)
-                print("You have a Blackjack, You won!")
-                play_game()
-            elif blackjack_check(dealer_cards) == 0:
-                show_score('final', player_cards, dealer_cards)
-                print("Dealer has BlackJack, You  lost")
-                play_game()
-            else:
-                show_score('final', player_cards, dealer_cards)
-                who_won(sum(player_cards), sum(dealer_cards))
-                play_game()
-        # start of the game
-        elif new_game == 'n':
-            playing = False
-            
-##################### Calling Program #####################
+    #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
 
-play_game()
+    for n in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    #Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
+    while not is_game_over:
+
+        #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f'Your cards {user_cards}, current_score: {user_score}')
+        print(f'Dealer\'s first card is {computer_cards[0]}')
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
+            user_should_deal = input('Do you want another card? y or n: ')
+            if user_should_deal == 'y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    #Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f'Your final hand is: {user_cards}, final score: {user_score}')
+    print(f'Dealer\'s final hand is: {computer_cards}, final score: {computer_score}')
+    print(compare(user_score, computer_score))
+
+#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+
+while input("Do you want to play a game of BlackJack? y or n: "):
+    play_game()
