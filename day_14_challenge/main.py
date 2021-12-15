26f1aeb458fbb9dc7e49f8d40b55934f13a66e2e
Alex M <alexmaksimets@gmail.com>
2021-12-15 21:07:35 +1000

    game working, refactored

--- day_14_challenge/main.py
+++ day_14_challenge/main.py
@@ -5,37 +5,26 @@ import art
 # display art
 print(art.logo)
 # import random module
-from random import randint
+from random import randint, choice
 # import game data
 from game_data import data
-
-# generate random celebrity
         
-def generate_celeb():
-    rand_num = randint(0, len(data)-1)
-    return data[rand_num]
-
 # both celebrities cannot be identical
 
 def generate_dif(celeb_a):
-    rand_num = randint(0, len(data)-1)
-    celeb = data[rand_num]
-    if celeb_a["follower_count"] == celeb["follower_count"]:
-        return generate_celeb()
-    else:
-        return celeb
+    celeb = choice(data)
+    while celeb_a["follower_count"] == celeb["follower_count"]:
+        celeb = choice(data)
+    return celeb
 
 # function that uses follower count to make comparison between A and B
-def compare_followers(a, b):
-    if a["follower_count"] > b["follower_count"]:
-        return 1
-    else:
-        return 0
 
-# function that keeps one of the variables from previous comparisson
-
-#def keep_one():
-    
+def check_answer(guess, a_followers, b_followers):
+  """take user's guess and follower counts and returns if got it right"""
+  if a_followers["follower_count"] > b_followers["follower_count"]:
+    return guess == "a"
+  else:
+    return guess == "b"  
     
 # create a variable equal to a randomly generated entry from data list
   # assign random celebrities to variables A and B
@@ -52,7 +41,7 @@ while not is_game_over:
         #clear()
         print(f"You're right! Current score: {total_score}")
     else:
-        var_a = generate_celeb()
+        var_a = choice(data)
     var_b = generate_dif(var_a)
 
     # print celebrities information: name, description, country
@@ -69,16 +58,12 @@ while not is_game_over:
 
     # assign answer to the correct variable
     # carry out comparison function
-    if answer == "a":
-        higher_lower = compare_followers(var_a, var_b)
-    elif answer == "b":
-        higher_lower = compare_followers(var_b, var_a)
     
-    if higher_lower < 1:
+    if check_answer(answer, var_a, var_b):
+        total_score += 1  
+    else:
         is_game_over = True
         print(f"Sorry, that's wrong. Final score: {total_score}")
-    else:
-        total_score += 1
         
         
 
