175262e6e4afc29b15c6180d302af1749aea446c
Alex M <alexmaksimets@gmail.com>
2021-12-15 19:47:13 +1000

    game logic working, but everytime generates random celebrities, needs to keep 1 from past

--- day_14_challenge/main.py
+++ day_14_challenge/main.py
@@ -32,50 +32,60 @@ def compare_followers(a, b):
     
 # create a variable equal to a randomly generated entry from data list
   # assign random celebrities to variables A and B
-  
+is_game_over = False  
 # use an array for variables?
 # variables = [var_a, var_b]
-
-variables = ["new", "different"]
-
-different = [variables.remove("different")]
-print(different)
-
-var_a = generate_celeb()
-var_b = generate_dif(var_a)
-
-# print celebrities information: name, description, country
-
-print(f'Compare A: {var_a["name"]}, a {var_a["description"]}, from {var_a["country"]}')
-
-print(art.vs)
-
-print(f'Against B: {var_b["name"]}, a {var_b["description"]}, from {var_b["country"]}')
-
-# ask who has more followers and use input to compare followers
-
-answer = input("Who has more followers? Type 'A' or 'B': ").lower()
-
-# create a score counter keeping track of all correct answers
-
 total_score = 0
 
-# carry out comparison function
-compare_followers(var_a, var_b)
+while not is_game_over:
+    var_a = generate_celeb()
+    var_b = generate_dif(var_a)
+
+    # print celebrities information: name, description, country
+
+    print(f'Compare A: {var_a["name"]}, a {var_a["description"]}, from {var_a["country"]}')
+
+    print(art.vs)
+
+    print(f'Against B: {var_b["name"]}, a {var_b["description"]}, from {var_b["country"]}')
+
+    # ask who has more followers and use input to compare followers
+
+    answer = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    # assign answer to the correct variable
+    if answer == "a":
+        higher_lower = compare_followers(var_a, var_b)
+    elif answer == "b":
+        higher_lower = compare_followers(var_b, var_a)
+    
+    if higher_lower < 1:
+        is_game_over = True
+        print(f"Sorry, that's wrong. Final score: {total_score}")
+    else:
+        total_score += 1
+        
+        
+
+    # create a score counter keeping track of all correct answers
 
 
-# if guessed correctly
-#if comparison function returns 1
-  # random varaible from previous game assigned as variable A against which new comparison made
-  # counter goes up by 1
-  # clear()
-  # wining message
-  # display current score
+
+    # carry out comparison function
 
 
-# if guessed wrong
-#if comparison function returns 0
-  # clear()
-  # losing message
-  # print final score
+    # if guessed correctly
+    #if comparison function returns 1
+      # random varaible from previous game assigned as variable A against which new comparison made
+      # counter goes up by 1
+      # clear()
+      # wining message
+      # display current score
+
+
+    # if guessed wrong
+    #if comparison function returns 0
+      # clear()
+      # losing message
+      # print final score
 
