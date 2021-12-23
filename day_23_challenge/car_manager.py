6b799d09a8f30c5c1ac21c5e35e06a22bd96456f
Alex M <alexmaksimets@gmail.com>
2021-12-23 13:08:26 +1000

    day 23 challenge TODO 4 complete

--- day_23_challenge/car_manager.py
+++ day_23_challenge/car_manager.py
@@ -3,7 +3,7 @@ from random import randint, choice
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+run = 1
 
 class CarManager:
     def __init__(self):
@@ -14,14 +14,22 @@ class CarManager:
         car = Turtle("square")
         car.color(choice(COLORS))
         car.penup()
-        car.speed(3)
         car.shapesize(stretch_wid=1, stretch_len=2)
-        car.goto(x=randint(20, 700), y=randint(-240, 240))
+        car.goto(x=randint(50, 700), y=randint(-240, 240))
         self.cars.append(car)
 
-    def move(self):
+    def move(self, inc=0):
+
         for car in self.cars:
-            car.backward(STARTING_MOVE_DISTANCE)
+            car.backward(STARTING_MOVE_DISTANCE + inc)
+
+    def speed_up(self):
+        global run
+        increment = MOVE_INCREMENT * run
+        self.move(increment)
+        run += 1
+        print(increment)
+
 
 
 
--- day_23_challenge/main.py
+++ day_23_challenge/main.py
@@ -33,13 +33,10 @@ while game_is_on:
     # detect if player has reached top edge
     if player.finished():
         player.reset_position()
+        car_generator.speed_up()
 
     screen.update()
     loop_num += 1
-
-# TODO 4: Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y).
-#  When this happens, return the turtle to the starting position and increase the speed of the cars. Hint: think
-#  about creating an attribute and using the MOVE_INCREMENT to increase the car speed.
 
 # TODO 5: Create a scoreboard that keeps track of which level the user is on. Every time the turtle player does a
 #  successful crossing, the level should increase. When the turtle hits a car, GAME OVER should be displayed in the
