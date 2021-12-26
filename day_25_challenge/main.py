9e139e29cba6f4517030b0192842fff3d8892fe9
Alex M <alexmaksimets@gmail.com>
2021-12-26 23:52:14 +1000

    day 25 challenge completed. cofe refactored

--- day_25_challenge/main.py
+++ day_25_challenge/main.py
@@ -8,10 +8,6 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-# # setup input field
-# guess_window = turtle.Turtle()
-# guess_window.textinput(title="Guess the State", prompt="What's another state name?")
-
 # database setup
 db = pandas.read_csv("50_states.csv")
 states_array = db.state.to_list()
@@ -21,22 +17,37 @@ map_turtle = turtle.Turtle()
 correct_guesses = []
 
 # set up input field
+# print(states_array)
 
-guessing = True
 # TODO: 4.
-while guessing:
+while len(correct_guesses) < 50:
     # TODO: 1 & 6
-    answer = screen.textinput(title=f"{len(correct_guesses)}/{len(states_array)} States Correct", prompt="What's another state name?").title()
+    answer = screen.textinput(title=f"{len(correct_guesses)}/50 States Correct", prompt="What's another state name?").title()
     # TODO: 2.
+    if answer == "Exit":
+        break
     if answer in states_array:
         # TODO: 3.
         row = db[db.state == answer]
-        map_turtle.penup()
         map_turtle.hideturtle()
-        map_turtle.goto(x=int(row.x + 1), y=int(row.y))
+        map_turtle.penup()
+        map_turtle.goto(x=int(row.x), y=int(row.y))
         map_turtle.write(arg=f"{answer}", move=False, align='center', font=('Arial', 8, 'normal'))
         # TODO: 5.
         correct_guesses.append(answer)
+
+missing_states = []
+
+for state in states_array:
+    if state not in correct_guesses:
+        missing_states.append(state)
+
+missing_states_dict = {
+    "states": missing_states
+}
+
+df = pandas.DataFrame(missing_states_dict)
+df.to_csv("states_to_learn.csv")
 
 
 # setup comparison logic
@@ -48,4 +59,3 @@ while guessing:
 # turtle.mainloop()
 # # replaces exitonclick()
 
-screen.exitonclick()
