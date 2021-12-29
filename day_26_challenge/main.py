5bb1c373ab92da144734c4d89e65b1f8536b2bfd
Alex M <alexmaksimets@gmail.com>
2021-12-29 10:09:19 +1000

    added error catching for phonetic code program

--- day_26_challenge/main.py
+++ day_26_challenge/main.py
@@ -1,24 +1,31 @@
+import pandas
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
-
-#Looping through dictionaries:
-for (key, value) in student_dict.items():
-    #Access key and value
-    pass
-
-import pandas
-student_data_frame = pandas.DataFrame(student_dict)
-
-#Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
-    pass
+# student_data_frame = pandas.DataFrame(student_dict)
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
 
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
+
+"""
+or 
+for letter in user_input:
+    for (key, value) in code_dict.items():
+        if letter is key:
+            output.append(value)
+"""
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 letter_df = pandas.DataFrame(data)
@@ -29,19 +36,18 @@ print(new_dict)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-# request input
-word = input("Type a name you want spelt out").upper()
-# print(list(user_input))
 
-# give output
-output = [new_dict[letter] for letter in word]
+def generate_code():
+    # request input
+    word = input("Enter a word\n").upper()
+    try:
+        # get output
+        output = [new_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry , only letters in the alphabet please")
+        generate_code()
+    else:
+        print(output)
 
-"""
-or 
-for letter in user_input:
-    for (key, value) in code_dict.items():
-        if letter is key:
-            output.append(value)
-"""
 
-print(output)
+generate_code()
