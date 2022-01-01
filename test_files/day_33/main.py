10659b476793540abf73b4239af2d808ae15a9eb
Alex M <alexmaksimets@gmail.com>
2022-01-01 15:39:54 +1000

    kanye app working

--- test_files/day_33/main.py
+++ test_files/day_33/main.py
@@ -1,11 +1,14 @@
 from tkinter import *
 import requests
+quote = ""
 
 
 def get_quote():
+    global quote
     response = requests.get(url="https://api.kanye.rest/")
-    data = response.json()
-    print(data["quote"])
+    quote = response.json()["quote"]
+    canvas.itemconfig(quote_text, text=quote)
+
 
 
 window = Tk()
@@ -21,7 +24,7 @@ canvas.grid(row=0, column=0)
 kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
-
 get_quote()
+
 
 window.mainloop()
