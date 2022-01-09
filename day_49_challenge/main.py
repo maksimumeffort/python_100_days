ca13437dfbc5d7a9cedf86c4d2e86c0b73beae2a
Alex M <alexmaksimets@gmail.com>
2022-01-09 15:01:59 +1000

    found a way into job descriptions

--- day_49_challenge/main.py
+++ day_49_challenge/main.py
@@ -19,23 +19,27 @@ driver.get(URL)
 sign_in = driver.find_element(By.CSS_SELECTOR, "div.nav__cta-container a.nav__button-secondary")
 sign_in.click()
 
-# time.sleep(5)
+time.sleep(3)
 
 user = driver.find_element(By.ID, "username")
 password = driver.find_element(By.ID, "password")
 submit = driver.find_element(By.CLASS_NAME, "btn__primary--large")
+
 user.send_keys(EMAIL)
+
 password.send_keys(PASS)
+
 submit.click()
 
-# run through the list of job ads and find the first one that has easy apply
+# OPTION 1: run through the list of job ads and find the first one that has easy apply
 
-search_results_list = driver.find_element(By.CSS_SELECTOR, "ul.jobs-search-results__list")
-search_result_items = search_results_list.find_elements(By.CSS_SELECTOR, "li.jobs-search-results__list-item")
-results = []
-for result in search_result_items:
-    res_id = result.get_attribute("id")
-    print(res_id)
+# search_results_list = driver.find_element(By.CSS_SELECTOR, "ul.jobs-search-results__list")
+# search_result_items = search_results_list.find_elements(By.CSS_SELECTOR, "li.jobs-search-results__list-item")
+# for result in search_result_items:
+#     res_id = result.get_attribute("id")
+#     print(res_id)
+#     # data_job_id = driver.find_element(By.XPATH, f"//*[@id={res_id}]/div/div").get_attribute("data-job-id")
+#     # print(data_job_id)
 
 
 
@@ -55,4 +59,20 @@ for result in search_result_items:
 #     btns_text_list.append(btn.find_element(By.CSS_SELECTOR, "span").text)
 # print(btns_text_list)
 
+## OPTION 2: click through the list, check if text == "easy apply", if not move to next item in list
+time.sleep(10)
+
+item_list = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")
+
+text_list = []
+for item in item_list:
+    time.sleep(2)
+    item.click()
+    item_desc = driver.find_element(By.CSS_SELECTOR, "div.jobs-unified-top-card__content--two-pane")
+    print("found description")
+    item_title = item_desc.find_element(By.TAG_NAME, "h2").text
+    text_list.append(item_title)
+# apply_btn = driver.find_element(By.CSS_SELECTOR, ".jobs-apply-button--top-card button span")
+print(text_list)
 driver.quit()
+
