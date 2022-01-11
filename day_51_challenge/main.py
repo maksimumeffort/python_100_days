72773042b37b28ce1046a2174f8ef69f28637277
Alex M <alexmaksimets@gmail.com>
2022-01-11 17:15:36 +1000

    added exception if twitter doesn't require phone verification

--- day_51_challenge/main.py
+++ day_51_challenge/main.py
@@ -12,11 +12,13 @@ DRIVER_PATH = "/Users/maximus/Documents/dev_doc/chromedriver"
 TWITTER_USER = os.environ["USER"]
 TWITTER_PASS = os.environ["PASS"]
 PHONE = os.environ["PHONE"]
+PROVIDER = "BELONG"
 S = Service(DRIVER_PATH)
 TWITTER_URL = "https://twitter.com/"
 SPEED_URL = "https://www.speedtest.net/"
 chrome_options = Options()
 chrome_options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15')
+
 
 class InternetSpeedTwitterBot:
     def __init__(self):
@@ -38,6 +40,10 @@ class InternetSpeedTwitterBot:
             else:
                 break
 
+    def generate_message(self):
+        self.message = f"Hey {PROVIDER}, why is my internet speed {self.down}down/{self.up}up " \
+                       f"when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
+
     def tweet_at_provider(self):
         print(self.up)
         print(self.down)
@@ -52,23 +58,42 @@ class InternetSpeedTwitterBot:
         username_input.send_keys(TWITTER_USER)
         next = self.driver.find_elements(By.CSS_SELECTOR, "div[role*='button']")[-3]
         next.click()
-        time.sleep(2)
-        # 2/3 verification
-        phone_input = self.driver.find_element(By.TAG_NAME, "input")
-        phone_input.send_keys(PHONE)
-        submit = self.driver.find_elements(By.CSS_SELECTOR, "div[role*='button']")[-1]
-        submit.click()
-        time.sleep(2)
-        # 3/3 verification
-        password_input = self.driver.find_elements(By.TAG_NAME, "input")[-1]
-        password_input.send_keys(TWITTER_PASS)
-        time.sleep(2)
-        login = self.driver.find_element(By.CSS_SELECTOR, "div[data-testid*='LoginForm_Login_Button']")
-        # print(login)
-        login.click()
-
-
-
+        time.sleep(5)
+        try:
+            # 2/3 verification
+            phone_input = self.driver.find_element(By.TAG_NAME, "input")
+            phone_input.send_keys(PHONE)
+        except:
+            # 3/3 verification
+            password_input = self.driver.find_elements(By.TAG_NAME, "input")[-1]
+            password_input.send_keys(TWITTER_PASS)
+            time.sleep(2)
+            login = self.driver.find_element(By.CSS_SELECTOR, "div[data-testid*='LoginForm_Login_Button']")
+            # print(login)
+            login.click()
+            # write a tweet
+            time.sleep(4)
+            self.generate_message()
+            text_field = self.driver.find_element(By.CSS_SELECTOR,
+                                                  "div.public-DraftStyleDefault-block public-DraftStyleDefault-ltr")
+            text_field.send_keys(self.message)
+            # 3/3 verification
+            password_input = self.driver.find_elements(By.TAG_NAME, "input")[-1]
+            password_input.send_keys(TWITTER_PASS)
+            time.sleep(2)
+            login = self.driver.find_element(By.CSS_SELECTOR, "div[data-testid*='LoginForm_Login_Button']")
+            # print(login)
+            login.click()
+            # write a tweet
+            time.sleep(4)
+            self.generate_message()
+            text_field = self.driver.find_element(By.CSS_SELECTOR,
+                                                  "div.public-DraftStyleDefault-block public-DraftStyleDefault-ltr")
+            text_field.send_keys(self.message)
+        else:
+            submit = self.driver.find_elements(By.CSS_SELECTOR, "div[role*='button']")[-1]
+            submit.click()
+            time.sleep(5)
 
 
 tracker = InternetSpeedTwitterBot()
