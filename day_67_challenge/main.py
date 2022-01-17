4a63ccf414d5fa1ddc919723984a274ee8a407a3
Alex M <alexmaksimets@gmail.com>
2022-01-17 18:56:47 +1000

    day 67 challenge 2.5/4 complete

---
+++ day_67_challenge/main.py
@@ -0,0 +1,115 @@
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask_ckeditor import CKEditor
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import datetime
+
+## Delete this code:
+# import requests
+# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+##CONFIGURE TABLE
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    author = StringField("Your Name", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+
+@app.route('/')
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
+    return render_template("post.html", post=requested_post)
+
+
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=requested_post.title,
+        subtitle=requested_post.subtitle,
+        author=requested_post.author,
+        img_url=requested_post.img_url,
+        body=requested_post.body,
+    )
+    if edit_form.validate_on_submit():
+        requested_post.title = edit_form.title.data
+        requested_post.subtitle = edit_form.subtitle.data
+        requested_post.body = edit_form.body.data
+        requested_post.author = edit_form.author.data
+        requested_post.img_url = edit_form.img_url.data
+        db.session.commit()
+        return redirect(url_for('show_post', post_id=requested_post.id))
+
+    return render_template("make-post.html", form=edit_form, edit=True)
+
+@app.route("/edit-post/<int:post_id>")
+def delete_post(post_id):
+    pass
+
+
+@app.route("/new-post", methods=["GET", "POST"])
+def create_post():
+    form = CreatePostForm()
+    date = datetime.now().date().strftime("%B %-d,%Y")
+    if form.validate_on_submit():
+        new_post = BlogPost(
+            title=request.form.get("title"),
+            subtitle=request.form.get("subtitle"),
+            date=date,
+            body=request.form.get('body'),
+            author=request.form.get("author"),
+            img_url=request.form.get("img_url"),
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for('get_all_posts'))
+    return render_template("make-post.html", form=form)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
