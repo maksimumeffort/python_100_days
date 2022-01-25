b1f4afc3626ac989e6d74d6c00c9ad00fb3dca80
Alex M <alexmaksimets@gmail.com>
2022-01-25 21:00:40 +1000

    bug found: easy apply indices array is not getting populated

--- day_49_challenge/main.py
+++ day_49_challenge/main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import os
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -8,8 +9,8 @@ path = "/Users/maximus/Documents/dev_doc/chromedriver"
 s = Service(path)
 driver = webdriver.Chrome(service=s)
 
-EMAIL = "maksdevil@hotmail.co.uk"
-PASS = "Throwaway2021@"
+EMAIL = os.environ["EMAIL"]
+PASS = os.environ["PASS"]
 PHONE = "459840931"
 
 URL = "https://www.linkedin.com/jobs/search/?currentJobId=2866113243&geoId=104001115&keywords=python%20developer&location=Brisbane%20City%2C%20QL"
@@ -27,6 +28,7 @@ submit = driver.find_element(By.CLASS_NAME, "btn__primary--large")
 user.send_keys(EMAIL)
 password.send_keys(PASS)
 submit.click()
+
 # OPTION 1: run through the list of job ads and find the first one that has easy apply
 
 # search_results_list = driver.find_element(By.CSS_SELECTOR, "ul.jobs-search-results__list")
@@ -62,7 +64,7 @@ def find_application_mode(item):
     item.click()
     item_desc = driver.find_element(By.CSS_SELECTOR, "div.jobs-unified-top-card__content--two-pane")
     # need to sleep so that "apply" element loads
-    time.sleep(1)
+    time.sleep(3)
     # item_title = item_desc.find_element(By.TAG_NAME, "h2").text
     item_spans = item_desc.find_elements(By.TAG_NAME, 'span')
     span_text_list = []
@@ -73,20 +75,26 @@ def find_application_mode(item):
         # makes sure applied jobs showing up in the list to not mess up the index of apply modes
         elif f"Applied" in span.text:
             span_text_list.append("Applied")
-    if len(span_text_list) != 1:
+    if len(span_text_list) >= 1:
         span_text_list.pop()
     apply_modes.append(span_text_list)
     # print(f"{span_text_list} added")
 
+# Check for Modal
+def modal_check():
+    modal = driver.find_element(By.CSS_SELECTOR, "div.artdeco-modal")
+    print(modal)
 
 time.sleep(10)
 
 # get a list of application modes
+
+applications_info_list = []
+
 search_results = driver.find_elements(By.CLASS_NAME, "jobs-search-results__list-item")
 apply_modes = []
 for result in search_results:
     find_application_mode(result)
-    # print("apply mode added")
 
 # find index of first list item with apply_mode = "Easy Apply"
 # flatten the apply_modes list
@@ -94,38 +102,55 @@ mode_list_flat = [item for sub_list in apply_modes for item in sub_list]
 
 # # find the first value in item_list with value "Easy Apply"
 easy_apply_indices = [n for n in range(len(mode_list_flat)) if mode_list_flat[n] == "Easy Apply"]
-# print(easy_apply_indices)
+print(easy_apply_indices)
 # print(mode_list_flat.index('Applied'))
-for index in easy_apply_indices:
+
+for index in easy_apply_indices[0:1]:
     search_results[index].click()
+    time.sleep(5)
+    title = driver.find_element(By.XPATH, "/html/body/div[6]/div[3]/div[3]/div[2]/div/section[2]/div/div/div[1]/div/div[1]/div")
+    print(title)
 
-    # # application process
-    apply_btn = driver.find_element(By.CSS_SELECTOR, ".jobs-apply-button--top-card button")
-    apply_btn.click()
-    time.sleep(2)
-
-    # find the phone input field and give phone value
-    phone_input = driver.find_element(By.TAG_NAME, "input")
-    phone_input.send_keys(PHONE)
-
-    # # click submit
-    # submit_button = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[2]/div/form/footer/div[3]/button")
-    # submit_button.click()
-
-    # click exit
-    exit_button = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/button")
-    exit_button.click()
-    discard_button = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/div[3]/button[1]")
-    discard_button.click()
-
-
-
-# needed_index = mode_list_flat.index('Easy Apply')
-# first_easy_app = search_results[needed_index]
-# first_easy_app.click()
-# print("found first easy app")
+# for index in easy_apply_indices:
+#     search_results[index].click()
+#     # # read application info
+#
+#     job_info = driver.find_element(By.CSS_SELECTOR, "div.jobs-unified-top-card")
+#     print(job_info)
+#
+#     job_title = job_info.find_element(By.CSS_SELECTOR, "h2")
+#     print(job_title)
+#
+#     # new_application = {
+#     #     "job_title":
+#     #     "Job location"
+#     #     "Method of Contact"
+#     #     "Employer Name"
+#     #     "Employer Contact"
+#     #     "Notes"
+#     # }
 #
 #
+#     # # application process
+#     apply_btn = driver.find_element(By.CSS_SELECTOR, ".jobs-apply-button--top-card button")
+#     apply_btn.click()
+#     time.sleep(2)
+#
+#     # find the phone input field and give phone value
+#     phone_input = driver.find_element(By.TAG_NAME, "input")
+#     phone_input.send_keys(PHONE)
+#
+#     # # click submit
+#     # submit_button = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div[2]/div/form/footer/div[3]/button")
+#     # submit_button.click()
+#
+#     # click exit
+#     exit_button = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/button")
+#     exit_button.click()
+#     discard_button = driver.find_element(By.XPATH, "/html/body/div[3]/div[2]/div/div[3]/button[1]")
+#     discard_button.click()
 
-driver.quit()
+
+
+# driver.quit()
 
